a32e0e72bde08c13fcdace759dfdef3cc580649f
Muh. Alif Anhar <sultankautsar21@gmail.com>
2025-05-21 11:41:55 +0800

    fix database dan modell

--- app/Models/TransactionPassenger.php
+++ app/Models/TransactionPassenger.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class TransactionPassenger extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'transaction_id',
+        'flight_seat_id',
+        'name',
+        'date_of_birth',
+        'nationality'
+    ];
 }
