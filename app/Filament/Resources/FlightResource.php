926cfec0952cd7f8b4080e5ad8b9e0155a62a2b1
Muh. Alif Anhar <sultankautsar21@gmail.com>
2025-05-22 13:46:48 +0800

    fix fitur flight

--- app/Filament/Resources/FlightResource.php
+++ app/Filament/Resources/FlightResource.php
@@ -23,7 +23,61 @@ class FlightResource extends Resource
     {
         return $form
             ->schema([
-                //
+                Forms\Components\Wizard::make([
+                    Forms\Components\Wizard\Step::make('Flight Information')
+                        ->schema([
+                            Forms\Components\TextInput::make('flight_number')
+                                ->required()
+                                ->unique(ignoreRecord: true),
+                            Forms\Components\Select::make('airline_id')
+                                ->required()
+                                ->relationship('airline', 'name'),
+                        ]),
+                    Forms\Components\Wizard\Step::make('Flight Segments')
+                        ->schema([
+                            Forms\Components\Repeater::make('flight_segments')
+                                ->relationship('segments')
+                                ->schema([
+                                    Forms\Components\TextInput::make('sequence')
+                                        ->numeric()
+                                        ->required(),
+                                    Forms\Components\Select::make('airport_id')
+                                        ->required()
+                                        ->relationship('airport', 'name'),
+                                    Forms\Components\DateTimePicker::make('time')
+                                        ->required(),
+                                ])
+                                ->collapsed(false)
+                                ->minItems(1),
+                        ]),
+                    Forms\Components\Wizard\Step::make('Flight Class')
+                        ->schema([
+                            Forms\Components\Repeater::make('flight_classes')
+                                ->relationship('classes')
+                                ->schema([
+                                    Forms\Components\Select::make('class_type')
+                                        ->options([
+                                            'economy' => 'Economy',
+                                            'business' => 'Business',
+                                        ])
+                                        ->required(),
+                                    Forms\Components\TextInput::make('price')
+                                        ->required()
+                                        ->numeric()
+                                        ->minValue(0)
+                                        ->prefix('IDR'),
+                                    Forms\Components\TextInput::make('total_seats')
+                                        ->required()
+                                        ->numeric()
+                                        ->minValue(1)
+                                        ->label('Total Seats'),
+                                    Forms\Components\Select::make('facilities')
+                                        ->relationship('facilities', 'name')
+                                        ->multiple()
+                                        ->required()
+                                ])
+                        ]),
+                ])->columnSpan(2),
             ]);
     }
 
@@ -31,13 +85,25 @@ class FlightResource extends Resource
     {
         return $table
             ->columns([
-                //
+                Tables\Columns\TextColumn::make('flight_number'),
+                Tables\Columns\TextColumn::make('airline.name'),
+                Tables\Columns\TextColumn::make('segments')
+                    ->label('Route & Duration')
+                    ->formatStateUsing(function (Flight $record): string {
+                        $firstSegment = $record->segments->first();
+                        $lastSegment = $record->segments->last();
+                        $route = $firstSegment->airport->iata_code . ' - ' . $lastSegment->airport->iata_code;
+                        $duration = (new \DateTime($firstSegment->time))->format('d F Y H:i') . ' - ' . (new \DateTime($lastSegment->time))->format('d F Y H:i');
+                        return $route . ' | ' . $duration;
+                    })
             ])
             ->filters([
                 //
             ])
             ->actions([
+                Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
