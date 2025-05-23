7245fb44bc0374f423cf0beb6654adb3e2198e29
Muh. Alif Anhar <sultankautsar21@gmail.com>
2025-05-23 18:55:18 +0800

    tambah hijauuuu

--- app/Interfaces/TransactionRepositoryInterface.php
+++ app/Interfaces/TransactionRepositoryInterface.php
@@ -5,7 +5,7 @@ namespace App\Interfaces;
 interface TransactionRepositoryInterface
 {
     public function getTransactionDataFromSession();
-    
+
 
     public function saveTransactionDataToSession($data);
 
@@ -17,6 +17,7 @@ interface TransactionRepositoryInterface
 
 
     public function getTransactionByCodeEmailPhone($code, $email, $phone);
+    
 }
 
 
