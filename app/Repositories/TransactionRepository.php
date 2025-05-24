60e608ac604bcd4f4e6054ced0cd62443e235032
Muh. Alif Anhar <sultankautsar21@gmail.com>
2025-05-24 12:08:01 +0800

    fix transaction repo

--- app/Repositories/TransactionRepository.php
+++ app/Repositories/TransactionRepository.php
@@ -16,5 +16,40 @@ class TransactionRepository implements TransactionRepositoryInterface
         return session()->get('transaction');
     }
 
-    
+    public function saveTransactionDataToSession($data)
+    {
+        $transaction = session()->get('transaction', []);
+
+        foreach ($data as $key => $value) {
+            $transaction[$key] = $value;
+        }
+
+        session()->put('transaction', $transaction);
+    }
+
+    public function saveTransaction($data)
+    {
+        $data['code'] = $this->generateTransactionCode();
+        $data['number_of_passengers'] = $this->countPassengers($data['passengers']);
+
+        // Hitung subtotal dan grand total awal
+        $data['subtotal'] = $this->calculateSubtotal($data['flight_class_id'], $data['number_of_passengers']);
+        $data['grandtotal'] = $data['subtotal'];
+
+        // Terapkan promo jika ada
+        if (!empty($data['promo_code'])) {
+            $data = $this->applyPromoCode($data);
+        }
+
+        // Tambahkan PPN
+        $data['grandtotal'] = $this->addPPN($data['grandtotal']);
+
+        // Simpan transaksi dan penumpang
+        $transaction = $this->createTransaction($data);
+        $this->savePassengers($data['passengers'], $transaction->id);
+
+        session()->forget('transaction');
+
+        return $transaction;
+    }
 }
