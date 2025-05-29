d3091de924904ce3716d331aab4fd8800da983f8
Muh. Alif Anhar <sultankautsar21@gmail.com>
2025-05-29 10:21:08 +0800

    setengah fix flight

--- app/Http/Controllers/FlightController.php
+++ app/Http/Controllers/FlightController.php
@@ -3,29 +3,36 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Interfaces\AirportRepositoryInterface;
 use App\Interfaces\AirlineRepositoryInterface;
 use App\Interfaces\FlightRepositoryInterface;
 
 class FlightController extends Controller
 {
+    private AirportRepositoryInterface $airportRepository;
     private AirlineRepositoryInterface $airlineRepository;
     private FlightRepositoryInterface $flightRepository;
 
-    public function __construct(AirlineRepositoryInterface $airlineRepository, FlightRepositoryInterface $flightRepository)
+    public function __construct(AirlineRepositoryInterface $airlineRepository, FlightRepositoryInterface $flightRepository, AirportRepositoryInterface $airportRepository)
     {
+        $this->airportRepository = $airportRepository;
         $this->airlineRepository = $airlineRepository;
-
-        $this->flightRepositoryy = $flightRepository;
+        $this->flightRepository = $flightRepository;
     }
     function index(Request $request)
     {
+        $departure = $this->airportRepository->getAirportByIataCode($request->departure);
+        $arrival = $this->airportRepository->getAirportByIataCode($request->arrival);
+
         $airlines = $this->airlineRepository->getAllAirlines();
 
         $flights = $this->flightRepository->getAllFlights([
-
+            'departure' => $departure->id ?? null,
+            'arrival' => $arrival->id ?? null,
+            'date' => $request->date ?? null,
         ]);
 
-        return view('pages.flight.index', compact('airlines'));
-        
+        return view('pages.flight.index', compact('flights', 'airlines'));
+
     }
 }
