28a61b20b78b443211d9d94302d71da3622fc6cf
Muh. Alif Anhar <sultankautsar21@gmail.com>
2025-05-31 14:57:47 +0800

    fix

--- resources/views/pages/flight/index.blade.php
+++ resources/views/pages/flight/index.blade.php
@@ -82,7 +82,6 @@
             <div id="Result" class="flex flex-col w-full h-fit rounded-3xl p-5 gap-5 bg-white">
                 <h2 class="font-bold text-xl leading-[30px]">Available Flights</h2>
                 @foreach ($flights as $flight)
-
                     @if ($flight->segments->count() > 2)
                         <div
                             class="transit-card accordion flex flex-col w-full rounded-[20px] border border-garuda-blue py-5 px-6 gap-5 overflow-hidden has-[:checked]:!h-[110px] has-[:checked]:border-[#E8EFF7] hover:!border-garuda-blue transition-all duration-300">
