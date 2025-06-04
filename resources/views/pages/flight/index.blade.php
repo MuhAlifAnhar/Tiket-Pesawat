bb913b5703c6b4e383a07534403d283584494d80
Muh. Alif Anhar <sultankautsar21@gmail.com>
2025-06-04 15:12:38 +0800

    tes tesssss

--- resources/views/pages/flight/index.blade.php
+++ resources/views/pages/flight/index.blade.php
@@ -119,8 +119,9 @@
                                     class="rounded-full py-3 px-5 text-center bg-garuda-blue hover:shadow-[0px_14px_30px_0px_#0068FF66] transition-all duration-300">
                                     <span class="font-semibold text-white">Choose</span>
                                 </a>
-                                
+
                             </label>
+                            
                             <hr class="border-[#E8EFF7]">
                             <div class="accordion-content flex justify-between">
                                 <div class="left-content flex flex-col gap-[10px]">
