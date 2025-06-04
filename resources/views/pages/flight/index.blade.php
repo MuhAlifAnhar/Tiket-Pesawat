9a98339131c3401a52e7fdbe3030a441398f586b
Muh. Alif Anhar <sultankautsar21@gmail.com>
2025-06-04 15:12:26 +0800

    tes tessss

--- resources/views/pages/flight/index.blade.php
+++ resources/views/pages/flight/index.blade.php
@@ -112,13 +112,14 @@
                                     </div>
                                     <p class="text-sm text-garuda-grey">Transit {{ $flight->segments->count() - 2 }}x</p>
                                 </div>
-                                
+
                                 <p class="min-w-[120px] font-semibold text-garuda-green text-center">
                                     {{ 'Rp ' . number_format($flight->classes->first()->price, 0, ',', '.') }}</p>
                                 <a href="choose-tiers.html"
                                     class="rounded-full py-3 px-5 text-center bg-garuda-blue hover:shadow-[0px_14px_30px_0px_#0068FF66] transition-all duration-300">
                                     <span class="font-semibold text-white">Choose</span>
                                 </a>
+                                
                             </label>
                             <hr class="border-[#E8EFF7]">
                             <div class="accordion-content flex justify-between">
