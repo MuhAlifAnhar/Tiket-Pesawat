0fcc28a59ea47a52c21608db1e46d34801c830bf
Muh. Alif Anhar <sultankautsar21@gmail.com>
2025-06-18 10:58:28 +0800

    fixx

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\FlightController;
 use App\Http\Controllers\BookingController;
 
 
+
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('/flight', [FlightController::class, 'index'])->name('flight.index');
