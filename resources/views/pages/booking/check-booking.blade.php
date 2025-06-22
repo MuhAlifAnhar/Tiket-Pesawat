c87dbb0e68528424f4582f9b3146dc61920f4cec
Muh. Alif Anhar <sultankautsar21@gmail.com>
2025-06-22 22:35:00 +0800

    fixx

--- resources/views/pages/booking/check-booking.blade.php
+++ resources/views/pages/booking/check-booking.blade.php
@@ -24,8 +24,9 @@
                                 class="appearance-none outline-none w-full font-semibold placeholder:font-normal"
                                 placeholder="Write your booking id">
                         </div>
-                        
+
                     </div>
+                    
                     <div class="flex flex-col gap-5">
                         <p class="font-semibold">Phone No.</p>
                         <div
