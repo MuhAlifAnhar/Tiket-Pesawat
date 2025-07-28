71f1fc5feca31c26ad855b0ae4731b6986273b42
Muh. Alif Anhar <sultankautsar21@gmail.com>
2025-07-28 10:06:39 +0800

    fixxx

--- app/Interfaces/AirportRepositoryInterface.php
+++ app/Interfaces/AirportRepositoryInterface.php
@@ -7,4 +7,5 @@ interface AirportRepositoryInterface
     public function getAllAirports();
     public function getAirportBySlug($slug);
     public function getAirportByIataCode($iatacode);
+    
 }
