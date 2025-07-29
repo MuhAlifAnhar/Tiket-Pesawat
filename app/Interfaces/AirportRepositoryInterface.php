9ee365fdf48fbcaf27c549652e6ade85b1700c32
Muh. Alif Anhar <sultankautsar21@gmail.com>
2025-07-29 13:35:28 +0800

    fix

--- app/Interfaces/AirportRepositoryInterface.php
+++ app/Interfaces/AirportRepositoryInterface.php
@@ -7,6 +7,7 @@ namespace App\Interfaces;
 interface AirportRepositoryInterface
 {
     public function getAllAirports();
+    
     public function getAirportBySlug($slug);
     public function getAirportByIataCode($iatacode);
 
